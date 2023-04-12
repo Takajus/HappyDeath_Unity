03012bf9f0758db6199d1e2c81c2f865126f330a
Thomas <thomas.takagi@edu.devinci.fr>
2023-04-12 13:21:55 +0200

    -FEATURES- [ADD] player day/night transformation

---
+++ Assets/Scripts/PlayerTransformation.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using LFS.FirstPerson.Gameplay;
+
+public class PlayerTransformation : MonoBehaviour
+{
+    [SerializeField, Required] GameObject playerDayModel, playerNightModel;
+    [SerializeField] private bool isDay = true;
+
+    [SerializeField] ParticleSystem transformationVFX;
+
+    void Start()
+    {
+        if (isDay)
+        {
+            playerDayModel.SetActive(true);
+            playerNightModel.SetActive(false);
+        }
+        else
+        {
+            playerDayModel.SetActive(false);
+            playerNightModel.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Test
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (isDay)
+                isDay = false;
+            else
+                isDay = true;
+
+            Transformation();
+        }
+
+    }
+
+    private void Transformation(/*bool timeIndex*/)
+    {
+        /*if (*//*timeIndex == *//*isDay)
+            return;*/
+
+        StartCoroutine("TransformationStart");
+    }
+
+    private IEnumerator TransformationStart()
+    {
+        PlayerController.Instance.DisablePlayer();
+
+        // Start Transition/Transformation VFX/Animation
+        ParticleSystem particule = transformationVFX;
+        particule.Play();
+
+        Debug.Log("Transformation Start");
+        yield return new WaitForSeconds(particule.main.duration / 2);
+        SwitchModel();
+        yield return new WaitForSeconds(particule.main.duration / 2);
+        Debug.Log("Transformation End");
+
+        PlayerController.Instance.EnablePlayer();
+    }
+
+    private bool TransformationEnd()
+    {
+        return true;
+    }
+
+    private void SwitchModel()
+    {
+        if (/*isDay*/!playerDayModel.activeInHierarchy)
+        {
+            playerDayModel.SetActive(true);
+            playerNightModel.SetActive(false);
+        }
+        else
+        {
+            playerDayModel.SetActive(false);
+            playerNightModel.SetActive(true);
+        }
+    }
+}
