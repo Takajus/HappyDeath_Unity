143fb918464f7549cf91ac78e347c8ac9e7ecdb1
Alice <alice.brayer@edu.devinci.fr>
2023-05-30 10:02:05 +0200

    [ART] prefabs tombstone cross

---
+++ Assets/Assets/enviro/tombstones/tomb1/indications_nomenclature.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class indications_nomenclature : MonoBehaviour
+{
+    // Nomenclature : pf_tb[catégorie de la tombe][stade de la tombe]_[numéro du préfab]
+
+    //catégorie de la tombe: 1) tombe croix, 2) tombe sablier, 3) "classique"
+
+    //stade de la tombe: 1) en bois, 2) avancée
+}
