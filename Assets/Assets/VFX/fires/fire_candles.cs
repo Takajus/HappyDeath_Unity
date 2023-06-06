b922c4f86a481adf604323cd0f0fdfc646452487
Alice <alice.brayer@edu.devinci.fr>
2023-06-06 11:14:47 +0200

    [ART] WIp script

--- Assets/Assets/VFX/fires/fire_candles.cs
+++ Assets/Assets/VFX/fires/fire_candles.cs
@@ -7,11 +7,16 @@ public class fire_candles : MonoBehaviour
     public List<GameObject> fires;
     private GameObject currentFire;
     public Vector3 offSet;
+    private GameObject goSpawner;
+    private bool isDay = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
         SpawnNewFire();
+        DayCycleEvents.OnDayStart += IsDay;
+        DayCycleEvents.OnNightStart += IsNight;
     }
 
     // Update is called once per frame
@@ -20,13 +25,19 @@ public class fire_candles : MonoBehaviour
         
     }
 
-
 	public void SpawnNewFire()
 	{
         DestroyCurrentFire();
-
-        int nombreChoisi = Random.Range(0, fires.Count);
-        currentFire = Instantiate(fires[nombreChoisi], transform.position + offSet, Quaternion.identity, transform);
+        if (isDay)
+        {
+            int nombreChoisi = Random.Range(1, fires.Count);
+            currentFire = Instantiate(fires[nombreChoisi], transform.position + offSet, Quaternion.identity, transform);
+        }
+        if (!isDay)
+        {
+            currentFire = Instantiate(fires[1], transform.position + offSet, Quaternion.identity, transform);
+        }
+        
     }    
 
 
@@ -35,4 +46,17 @@ public class fire_candles : MonoBehaviour
         if (currentFire)
             Destroy(currentFire);
     }
+
+    private void IsDay()
+    {
+        isDay = false;
+        //changement de color jour 
+    }
+
+    private void IsNight()
+    {
+        isDay = true;
+        //changement de color nuit
+    }
+
 }
