acf326a3a45065714cae64b2874a3e8f005d6ee3
Wiloux <54811449+Wiloux@users.noreply.github.com>
2023-06-06 10:27:03 +0200

    Update fire_candles.cs

--- Assets/Assets/VFX/fires/fire_candles.cs
+++ Assets/Assets/VFX/fires/fire_candles.cs
@@ -4,43 +4,35 @@ using UnityEngine;
 
 public class fire_candles : MonoBehaviour
 {
-    private GameObject moimoi;
-
-    public int nombreDeFeuxPossibles;
-
-    public GameObject fire_1;
-    public GameObject fire_2;
-    public GameObject fire_3;
-    public GameObject fire_4;
-
-    private int nombreChoisi;
+    public List<GameObject> fires;
+    private GameObject currentFire;
+    public Vector3 offSet;
 
     // Start is called before the first frame update
     void Start()
     {
-        nombreChoisi = Random.Range(1, nombreDeFeuxPossibles);
-
-        switch(nombreChoisi)
-        {
-            case 1:
-                Instantiate(fire_1, transform.gameObject);
-                break;
-            case 2:
-                Instantiate(fire_2, moimoi.transform.position, moimoi.transform.rotation);
-                break;
-            case 3:
-                Instantiate(fire_3, moimoi.transform.position, moimoi.transform.rotation);
-                break;
-            case 4:
-                Instantiate(fire_4, moimoi.transform.position, moimoi.transform.rotation);
-                break;
-
-        }
+        SpawnNewFire();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+	public void SpawnNewFire()
+	{
+        DestroyCurrentFire();
+
+        int nombreChoisi = Random.Range(0, fires.Count);
+        currentFire = Instantiate(fires[nombreChoisi], transform.position + offSet, Quaternion.identity, transform);
+    }    
+
+
+    public void DestroyCurrentFire()
+    {
+        if (currentFire)
+            Destroy(currentFire);
     }
 }
