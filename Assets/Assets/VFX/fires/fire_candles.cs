5b5130d1d6a062739dcb6b673e03ae14b62b100f
Wiloux <54811449+Wiloux@users.noreply.github.com>
2023-06-06 11:28:56 +0200

    :fire: :candy:

--- Assets/Assets/VFX/fires/fire_candles.cs
+++ Assets/Assets/VFX/fires/fire_candles.cs
@@ -4,59 +4,54 @@ using UnityEngine;
 
 public class fire_candles : MonoBehaviour
 {
-    public List<GameObject> fires;
-    private GameObject currentFire;
-    public Vector3 offSet;
-    private GameObject goSpawner;
-    private bool isDay = false;
+	public List<GameObject> fires;
+	private GameObject currentFire;
+	public Vector3 offSet;
+	private GameObject goSpawner;
+	private bool isDay = false;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        SpawnNewFire();
-        DayCycleEvents.OnDayStart += IsDay;
-        DayCycleEvents.OnNightStart += IsNight;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-	public void SpawnNewFire()
+	// Start is called before the first frame update
+	void Start()
 	{
-        DestroyCurrentFire();
-        if (isDay)
-        {
-            int nombreChoisi = Random.Range(1, fires.Count);
-            currentFire = Instantiate(fires[nombreChoisi], transform.position + offSet, Quaternion.identity, transform);
-        }
-        if (!isDay)
-        {
-            currentFire = Instantiate(fires[1], transform.position + offSet, Quaternion.identity, transform);
-        }
-        
-    }    
+		SpawnNewFire(false);
+		DayCycleEvents.OnDayStart += IsDay;
+		DayCycleEvents.OnNightStart += IsNight;
+	}
+
+	// Update is called once per frame
+
+	public void SpawnNewFire(bool isDay)
+	{
+		DestroyCurrentFire();
+
+		if (isDay)
+		{
+		currentFire = Instantiate(fires[0], transform.position + offSet, Quaternion.identity, transform);
+			return;
+		}
+		int nombreChoisi = Random.Range(0, fires.Count);
+		currentFire = Instantiate(fires[nombreChoisi], transform.position + offSet, Quaternion.identity, transform);
+
+	}
 
 
-    public void DestroyCurrentFire()
-    {
-        if (currentFire)
-            Destroy(currentFire);
-    }
+	public void DestroyCurrentFire()
+	{
+		if (currentFire)
+			Destroy(currentFire);
+	}
 
-    private void IsDay()
-    {
-        isDay = false;
-        //changement de color jour 
-    }
+	private void IsDay()
+	{
+		SpawnNewFire(false);
+		//changement de color jour 
+	}
 
-    private void IsNight()
-    {
-        isDay = true;
-        //changement de color nuit
-    }
+	private void IsNight()
+	{
+		SpawnNewFire(true);
+		//changement de color nuit
+	}
 
 }
