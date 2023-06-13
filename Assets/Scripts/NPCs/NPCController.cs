f1a26e482cdd340d7db1b65a643d5752a344bd4c
Zyrgal <Valud64@hotmail.fr>
2023-06-13 12:07:52 +0200

    -IA- [ADD] Rotation for NPC

--- Assets/Scripts/NPCs/NPCController.cs
+++ Assets/Scripts/NPCs/NPCController.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,7 +8,7 @@ public class NPCController : MonoBehaviour
     public NavMeshAgent agent;
     public float range;
     public bool shouldMove;
-    private bool isMoving;
+    private bool canMove = true;
     private float pauseTimeAmount = 0f;
 
     public Transform areaCenter;
@@ -25,26 +25,24 @@ public class NPCController : MonoBehaviour
     {
         if (shouldMove)
         {
-            if (isMoving)
-                animator.SetBool("Walking", true);
-            else
-                animator.SetBool("Walking", false);
-
-            if (agent.remainingDistance <= agent.stoppingDistance)
+            //animator.SetBool("Walking", isMoving);
+            if (agent.remainingDistance <= agent.stoppingDistance && canMove)
             {
                 Vector3 point;
+                animator.SetBool("Walking", false);
 
                 if (pauseTimeAmount >= 0)
                 {
-                    isMoving = false;
+                    //isMoving = false;
                     Debug.Log(pauseTimeAmount);
                     pauseTimeAmount -= Time.deltaTime;
                 }
                 else if (RandomPoint(areaCenter.position, range, out point))
                 {
                     Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
-                    agent.SetDestination(point);
-                    isMoving = true;
+                    //agent.SetDestination(point);
+                    //isMoving = true;
+                    StartCoroutine(RotateTowards(point));
                     pauseTimeAmount = 2f;
                 }
             }
@@ -52,7 +50,7 @@ public class NPCController : MonoBehaviour
     }
     bool RandomPoint(Vector3 center, float range, out Vector3 result)
     {
-        Vector3 randomPoint = center + Random.insideUnitSphere * range;
+        Vector3 randomPoint = center + UnityEngine.Random.insideUnitSphere * range;
         NavMeshHit hit;
         if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
         {
@@ -62,5 +60,34 @@ public class NPCController : MonoBehaviour
 
         result = Vector3.zero;
         return false;
+    }
+
+    IEnumerator RotateTowards(Vector3 point)
+    {
+        canMove = false;
+        float turnSpeed = 3;
+
+        Vector3 direction = (new Vector3(point.x, transform.position.y, point.z) - transform.position).normalized;
+        float elaspedTime = 0;
+        float duration = Mathf.Abs(Vector3.Angle(transform.forward, direction) / 180) * turnSpeed;
+
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = Quaternion.LookRotation(direction, Vector3.up);
+
+
+        animator.SetBool("Walking", true);
+        while (elaspedTime < duration) 
+        {
+            float percentage = Mathf.SmoothStep(0, 1 ,elaspedTime / duration);
+
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, percentage);
+
+            elaspedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        agent.SetDestination(point);
+        canMove = true;
+        yield return null;
     }
 }
