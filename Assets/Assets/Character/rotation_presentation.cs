d897e8b70dc45e3cf12cbf9e22248edccea41bca
Alice <alice.brayer@edu.devinci.fr>
2023-06-15 23:51:00 +0200

    [ART] script de rotation de GO (pour la présentation)

---
+++ Assets/Assets/Character/rotation_presentation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation_presentation : MonoBehaviour
+{
+    public float vitesseRotation =1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rotationVector = transform.rotation.eulerAngles;
+        rotationVector.z = rotationVector.z + vitesseRotation;
+        gameObject.transform.rotation = Quaternion.Euler(rotationVector);
+    }
+}
